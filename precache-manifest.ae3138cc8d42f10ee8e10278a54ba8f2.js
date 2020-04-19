9276a693cde423fd3201144c5aec5a20b6ebfab0
sriramarul <sriramarul01@gmail.com>
2020-04-19 10:45:14 +0530

    Updates

---
+++ precache-manifest.ae3138cc8d42f10ee8e10278a54ba8f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b2e4aed2ffaf49e8af92e6fc9e4521c",
+    "url": "/deploy-reactjs/index.html"
+  },
+  {
+    "revision": "c17e88e602bfc11a0afa",
+    "url": "/deploy-reactjs/static/css/main.7d6c26fe.chunk.css"
+  },
+  {
+    "revision": "10b64e6cea0e41d80314",
+    "url": "/deploy-reactjs/static/js/2.3db4cd7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-reactjs/static/js/2.3db4cd7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c17e88e602bfc11a0afa",
+    "url": "/deploy-reactjs/static/js/main.eba24d84.chunk.js"
+  },
+  {
+    "revision": "6c9d2953390cc80d402d",
+    "url": "/deploy-reactjs/static/js/runtime-main.c057781b.js"
+  }
+]);
